bc76e2ad8e929ff31f267674b1b5926828c0beb2
Sai Siddartha, Alleni <allensaisiddharth@gmail.com>
2026-01-13 01:10:56 +0000

    changes added to relocate imu

--- scripts/skrl/play.py
+++ scripts/skrl/play.py
@@ -113,7 +113,7 @@ import isaaclab_tasks  # noqa: F401
 from isaaclab_tasks.utils import get_checkpoint_path
 from isaaclab_tasks.utils.hydra import hydra_task_config
 
-import temp.tasks  # noqa: F401
+import basic.tasks  # noqa: F401
 
 # config shortcuts
 if args_cli.agent is None:
