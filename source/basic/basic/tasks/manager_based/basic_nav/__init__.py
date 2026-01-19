d747e4ba5699864e802f0fc970bb83149638fd93
Sai Siddartha, Alleni <allensaisiddharth@gmail.com>
2026-01-19 14:18:38 -0500

    register obstacle navigation task

--- source/basic/basic/tasks/manager_based/basic_nav/__init__.py
+++ source/basic/basic/tasks/manager_based/basic_nav/__init__.py
@@ -21,3 +21,13 @@ gym.register(
         "skrl_cfg_entry_point": f"{agents.__name__}:basic_nav_policy_cfg.yaml",
     },
 )
+
+gym.register(
+    id="Template-Obstacle-Nav-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.obstacle_nav_env_cfg:ObstacleNavEnvCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:basic_nav_policy_cfg.yaml",
+    },
+)
